64b8ab0e9898f90d48ee6533c1339168658b2978
Vitaliy Ramazanov <ram010172@yandex.ru>
2020-10-09 13:11:24 +0300

    Project init

---
+++ CarMarket/CarMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarMarket
+//
+//  Created by Виталий Рамазанов on 09.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
