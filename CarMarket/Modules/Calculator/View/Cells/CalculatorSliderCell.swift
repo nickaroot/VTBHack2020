7e181e20775ac603484aa5efba5f9d6919cc4151
Vitaliy Ramazanov <ram010172@yandex.ru>
2020-10-10 21:28:13 +0300

    донастроил slidercell

--- CarMarket/Modules/Calculator/View/Cells/CalculatorSliderCell.swift
+++ CarMarket/Modules/Calculator/View/Cells/CalculatorSliderCell.swift
@@ -14,13 +14,20 @@ struct CalculatorSliderCellDatasource: CalculatorCellDatasource {
     var currentValue: Int
 }
 
+enum CalculatorSliderDataType {
+    case money
+    case months
+}
+
 class CalculatorSliderCell: UITableViewCell, CalculatorBaseCell {
+    @IBOutlet weak var cellTitleLabel: UILabel!
     @IBOutlet weak var currentValueLabel: UILabel!
     @IBOutlet weak var minValueLabel: UILabel!
     @IBOutlet weak var maxValueLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     
     var type: CalculatorCellType?
+    var dataType: CalculatorSliderDataType = .money
     var datasource: CalculatorCellDatasource? {
         didSet {
             if let datasource = datasource as? CalculatorSliderCellDatasource {
@@ -42,20 +49,53 @@ class CalculatorSliderCell: UITableViewCell, CalculatorBaseCell {
     }
     
     private func reloadData(with datasource: CalculatorSliderCellDatasource) {
-        minValueLabel.text = "\(datasource.minValue)"
-        maxValueLabel.text = "\(datasource.maxValue)"
-        currentValueLabel.text = "\(datasource.currentValue)"
+        switch datasource.type {
+        case .slider(let type):
+            switch type {
+            case .price:
+                cellTitleLabel.text = "Стоимость"
+                dataType = .money
+            case .income:
+                cellTitleLabel.text = "Первоначальный взнос"
+                dataType = .money
+            case .length:
+                cellTitleLabel.text = "Срок кредита"
+                dataType = .months
+            default:
+                break
+            }
+        default:
+            break
+        }
+        
+        minValueLabel.text = formattedValue(withValue: datasource.minValue) ?? "\(datasource.minValue)"
+        maxValueLabel.text = formattedValue(withValue: datasource.maxValue) ?? "\(datasource.maxValue)"
+        currentValueLabel.text = formattedValue(withValue: datasource.currentValue) ?? "\(datasource.currentValue)"
         
         slider.minimumValue = Float(datasource.minValue)
         slider.maximumValue = Float(datasource.maxValue)
         slider.value = Float(datasource.currentValue)
     }
     
+    private func formattedValue(withValue value: Int) -> String? {
+        if dataType == .money {
+            let formatter = NumberFormatter()
+            formatter.locale = Locale(identifier: "ru-RU")
+            formatter.maximumFractionDigits = 0
+            formatter.currencySymbol = Locale(identifier: "ru-RU").currencySymbol
+            formatter.numberStyle = .currency
+            return formatter.string(from: NSNumber(integerLiteral: value))
+        } else {
+            return "\(value) месяцев"
+        }
+    }
+    
+    
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        let newValue = Int(sender.value)
+        let newValue = dataType == .money ? Int(round(sender.value / 1000) * 1000) : Int(sender.value)
         if var datasource = datasource as? CalculatorSliderCellDatasource {
             datasource.currentValue = newValue
         }
-        currentValueLabel.text = "\(newValue)"
+        currentValueLabel.text = formattedValue(withValue: newValue) ?? "\(newValue)"
     }
 }
