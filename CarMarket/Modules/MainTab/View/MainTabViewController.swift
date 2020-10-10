7746c379478527df898d61e79a18ee563426247a
Vitaliy Ramazanov <ram010172@yandex.ru>
2020-10-10 12:31:30 +0300

    Накидал главный экран (частично)

--- CarMarket/Modules/MainTab/View/MainTabViewController.swift
+++ CarMarket/Modules/MainTab/View/MainTabViewController.swift
@@ -9,14 +9,45 @@
 import UIKit
 
 class MainTabViewController: UIViewController {
-
+    // MARK: IBOutlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var parameterSearchView: UIView!
+    @IBOutlet weak var photoSearchView: UIView!
+    
     // MARK: Properties
     var interactor: MainTabInteractorProtocol!
 
     // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+        configureGestures()
+        
+        interactor.viewIsReady()
     }
+    
+    private func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
+    
+    private func configureGestures() {
+        parameterSearchView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(parameterSearchClicked)))
+        photoSearchView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(photoSearchClicked)))
+        
+    }
+    
+    @objc func parameterSearchClicked() {
+        interactor.parameterSearchClicked()
+    }
+    
+    @objc func photoSearchClicked() {
+        interactor.photoSearchClicked()
+    }
+    
 }
 
 extension MainTabViewController: MainTabViewProtocol {
