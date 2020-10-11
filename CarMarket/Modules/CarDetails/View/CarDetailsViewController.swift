f72eb03d29f93886598450017d52b22c3a85be7b
Vitaliy Ramazanov <ram010172@yandex.ru>
2020-10-11 09:33:22 +0300

    -//-

--- CarMarket/Modules/CarDetails/View/CarDetailsViewController.swift
+++ CarMarket/Modules/CarDetails/View/CarDetailsViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class CarDetailsViewController: UIViewController {
     @IBOutlet weak var carPhotoCarousel: DazzleCarouselView!
+    @IBOutlet weak var carNameLabel: UILabel!
+    @IBOutlet weak var yandexImage: UIImageView!
+    @IBOutlet weak var carPriceLabel: UILabel!
+    
+    @IBOutlet weak var yaerLabel: UILabel!
+    @IBOutlet weak var transmissionLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var engineLabel: UILabel!
+    @IBOutlet weak var tabLabel: UILabel!
+    @IBOutlet weak var wheelDriveLabel: UILabel!
+    @IBOutlet weak var wheelLabel: UILabel!
     
     // MARK: Properties
     var interactor: CarDetailsInteractorProtocol!
@@ -32,6 +43,9 @@ class CarDetailsViewController: UIViewController {
     }
     
     private func configureUI() {
+        carPhotoCarousel.backgroundColor = .clear
+        yandexImage.layer.cornerRadius = 16
+        
         navigationController?.navigationBar.isHidden = false
         navigationItem.setHidesBackButton(true, animated: false)
         navigationController?.hidesBottomBarWhenPushed = true
@@ -66,10 +80,12 @@ extension CarDetailsViewController: CarDetailsViewProtocol {
 
 extension CarDetailsViewController: DazzleCarouselViewDatasource {
     func numberOfViewsToShow() -> Int {
-        return 0
+        return 3
     }
     
     func view(at index: Int) -> UIView {
-        return UIView()
+        let card = PhotoTextCard()
+        card.datasource = PhotoTextCardDatasource(id: UUID.init().uuidString, photo: #imageLiteral(resourceName: "CarPhoto"), text: nil)
+        return card
     }
 }
